b8f23f131559e2593a28997ba130a3b3347b4f49
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-11-27 23:24:01 +0300

    Add "npm run watch" script

--- js/components/BookList.jsx
+++ js/components/BookList.jsx
@@ -6,7 +6,7 @@ export default function BookList() {
   return (
     <div>
       <Book title="Lord of the Buffer Rings" titleColor="pink" />
-      <Book title="Harry Potter and the Virtual DOM" description="A handy book!" />
+      <Book title="Harry Potter and the Virtual DOM" description="A handy book indeed!" />
       <Book title="30 Days without jQuery" />
       <Book title="7 Habits of Highly Effective Procrastinators" />
     </div>
