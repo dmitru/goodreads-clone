619260d93df722173aff89a70e3b0e50f53e8fd2
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-11-28 12:33:47 +0300

    Load global CSS files with Webpack + HMR

--- js/index.jsx
+++ js/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import '../styles/global.css';
 
 const renderApp = () => {
   ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
         test: /\.jsx?$/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
