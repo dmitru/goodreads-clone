80b49e35ec6a6f4626e07aee2f0933e905f0525f
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-11-28 12:22:02 +0300

    Use styled-components for styling

--- js/components/Book.jsx
+++ js/components/Book.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { BookShape } from '../shapes';
 import getRandomColor from '../util/getRandomColor';
 
+const BookWrapper = styled.div`
+  border: 1px solid black;
+  margin: 0.5rem;
+  padding: 0.5rem;
+`;
+
+const BookTitle = styled.h2`
+  color: ${props => props.color || getRandomColor()};
+`;
+
 export default function Book({ title, description, titleColor }) {
-  const titleStyle = {
-    color: titleColor || getRandomColor(),
-  };
-
-  const bookWrapperStyle = {
-    border: '1px solid black',
-    margin: '0.5rem',
-    padding: '0.5rem',
-  };
-
   return (
-    <div style={bookWrapperStyle}>
-      <h2 style={titleStyle}>{title}</h2>
+    <BookWrapper>
+      <BookTitle color={titleColor}>{title}</BookTitle>
       {description ? <p>{description}</p> : <em>No description :(</em>}
-    </div>
+    </BookWrapper>
   );
 }
 
